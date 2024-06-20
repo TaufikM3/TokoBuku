25798d5c2836cb8c4cb110fd13cc5285b8667d7e
TaufikM3 <341.mth@gmail.com>
2024-06-20 09:59:09 +0700

    pembuatan navbar

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-layout>
+    <v-app-bar :elevation="3" rounded>
+      <v-app-bar-title>BookuHub</v-app-bar-title>
+      <v-text-field
+      variant="outlined"
+      hide-details="auto"
+      density="comfortable"
+      label="search"
+      append-inner-icon="mdi-magnify"
+      ></v-text-field>
+    </v-app-bar> 
+  </v-layout>
+</template>
